4a14eabca4c9e66fbfcd4a85362cf533534d3a0c
kaki005 <u097498f@ecs.osaka-u.ac.jp>
2024-08-10 23:45:25 +0900

    edit Tensorplot

--- src/main.py
+++ src/main.py
@@ -6,17 +6,18 @@ from configs import Config
 from tensor_plot import TensorPlot
 
 
-def sin_func(t: np.ndarray, omega: int):
+def sin_func(t: np.ndarray, omega: float):
     return np.sin(omega * t)
 
 
-# @hydra.main(version_base=None, config_path="configs/", config_name="default")
+@hydra.main(version_base=None, config_path="configs/", config_name="default")
 def main(cfg: Config):
     times = np.arange(1, 100)
-    tensors = np.array([sin_func(times, i * j) for j in range(3) for i in range(3)])
+    tensors = np.array([[sin_func(times, 0.1 * i * j) for j in range(3)] for i in range(3)])
+    print("tensors=", tensors.shape)
     tensors = tensors.transpose(2, 1, 0)
     plot = TensorPlot()
-    plot.plot_tensor(tensors, "test.png")
+    plot.plot_tensor(tensors, "outpus/test.png")
 
 
 if __name__ == "__main__":
--- src/tensor_plot/TensorPlot.py
+++ src/tensor_plot/TensorPlot.py
@@ -1,29 +1,36 @@
 import io
+from typing import Dict, List, Tuple
 
+import cv2 as cv
 import matplotlib
-import numpy as np
-import opencv2 as cv
-from matplotlib.figure import Figure
-
-matplotlib.use("agg")
-from typing import dict, list, tuple
-
 import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.figure import Figure
+from PIL import Image as im
+from PIL.Image import Image
+
+colorlist = ["r", "g", "b", "c", "m", "y", "k", "w"]
+matplotlib.use("agg")
 
 
 class TensorPlot:
-    def plot_tensor(self, tensor: np.ndarray, save_path: str, shift: tuple[int, int] = (10, 10), dpi: int = 100):
-        overall_img = np.zeros((4 * dpi, 3 * dpi, 3))
-        for i in range(tensor.shape[1]):
+    def plot_tensor(self, tensor: np.ndarray, save_path: str, shift: tuple[int, int] = (30, 30), dpi: int = 100):
+        series_num = tensor.shape[1]
+        overall_img = im.new(
+            "RGBA", (4 * dpi + shift[0] * series_num, 3 * dpi + shift[1] * series_num), (255, 255, 255, 255)
+        )
+        for i in range(series_num):
             fig: Figure = plt.figure(figsize=(4, 3), dpi=dpi)
             ax = fig.add_subplot(111)  # one graph
-            ax.plot(range(tensor.shape[0]), tensor[:, i])
-            img_array = self._plt_to_ndarray(fig)
-            self._overlay(img_array, overall_img, shift=(shift[0] * i, shift[1] * i))
-        cv.imwrite(save_path, overall_img)
+            ax.plot(range(tensor.shape[0]), tensor[:, i], color=colorlist[i])
+            ax.tick_params(labelbottom=False, labelleft=False, labelright=False, labeltop=False)
+            image = self._plt_to_image(fig)
+            image = self._transparent(image)
+            overall_img = self._overlay(image, overall_img, shift=(shift[0] * i, shift[1] * i))
+        overall_img.save(save_path)
 
     @staticmethod
-    def _plt_to_ndarray(fig: Figure, dpi: int = 100) -> np.ndarray:
+    def _plt_to_image(fig: Figure, dpi: int = 100) -> Image:
         fig.canvas.draw()
         # Now we can save it to a numpy array.
         buf = io.BytesIO()  # インメモリのバイナリストリームを作成
@@ -35,37 +42,43 @@ class TensorPlot:
         buf.close()  # ストリームを閉じる(flushする)
         img = cv.imdecode(img_arr, 1)  # 画像のバイナリデータを復元する
         img = cv.cvtColor(img, cv.COLOR_BGR2RGB)  # cv2.imread() はBGR形式で読み込むのでRGBにする.
-        return img
+        return im.fromarray(img)
 
     @staticmethod
-    def _overlay(fore_img: np.ndarray, back_img: np.ndarray, shift: tuple[int, int]) -> np.ndarray:
+    def select_color(color):
+        mean = np.array(color).mean(axis=0)
+        return (255, 255, 255, 0) if mean >= 250 else color
+
+    def _transparent(self, src: Image) -> Image:
+        src = src.convert("RGBA")
+        w, h = src.size
+        for y in range(h):
+            for x in range(w):
+                src.putpixel((x, y), self.select_color(src.getpixel((x, y))))
+        return src
+
+    @staticmethod
+    def _overlay(fore_img: Image, back_img: Image, shift: tuple[int, int]) -> Image:
         """
         fore_img：合成する画像
         back_img：背景画像
         shift：左上を原点としたときの移動量(x, y)
         """
+        composite_img = im.new("RGBA", back_img.size, (255, 255, 255, 0))
+        composite_img.paste(fore_img, shift, fore_img)
+        result_image = im.alpha_composite(back_img, composite_img)
+        return result_image
+        # shift_x, shift_y = shift
+        # fore_h, fore_w = fore_img.shape[:2]
+        # fore_x_min, fore_x_max = 0, fore_w
+        # fore_y_min, fore_y_max = 0, fore_h
 
-        shift_x, shift_y = shift
-        fore_h, fore_w = fore_img.shape[:2]
-        fore_x_min, fore_x_max = 0, fore_w
-        fore_y_min, fore_y_max = 0, fore_h
-
-        back_h, back_w = back_img.shape[:2]
-        back_x_min, back_x_max = shift_y, shift_y + fore_h
-        back_y_min, back_y_max = shift_x, shift_x + fore_w
-
-        if back_x_min < 0:
-            fore_x_min = fore_x_min - back_x_min
-            back_x_min = 0
-        if back_x_max > back_w:
-            fore_x_max = fore_x_max - (back_x_max - back_w)
-            back_x_max = back_w
-        if back_y_min < 0:
-            fore_y_min = fore_y_min - back_y_min
-            back_y_min = 0
-        if back_y_max > back_h:
-            fore_y_max = fore_y_max - (back_y_max - back_h)
-            back_y_max = back_h
-
-        back_img[back_y_min:back_y_max, back_x_min:back_x_max] = fore_img[fore_y_min:fore_y_max, fore_x_min:fore_x_max]
-        return back_img
+        # back_h, back_w = back_img.shape[:2]
+        # back_x_min, back_x_max = shift_y, shift_y + fore_w
+        # back_y_min, back_y_max = shift_x, shift_x + fore_h
+        # print(f"back_x=[{back_x_min}, {back_x_max}]")
+        # print(f"back_y=[{back_y_min}, {back_y_max}]")
+        # print(f"fore_x=[{fore_x_min}, {fore_x_max}]")
+        # print(f"fore_y=[{fore_y_min}, {fore_y_max}]")
+        # back_img[back_y_min:back_y_max, back_x_min:back_x_max, mask] = fore_img[fore_y_min:fore_y_max, fore_x_min:fore_x_max, mask]
+        # return back_img
