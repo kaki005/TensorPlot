edf9538455c7c98d1fd574c39afd294220cf772c
kaki005 <u097498f@ecs.osaka-u.ac.jp>
2024-08-11 23:42:24 +0900

    r

--- src/main.py
+++ src/main.py
@@ -1,8 +1,4 @@
-import hydra
 import numpy as np
-import omegaconf
-import wandb
-from configs import Config
 from tensor_plot import TensorPlot
 
 
@@ -10,13 +6,12 @@ def sin_func(t: np.ndarray, omega: float):
     return np.sin(omega * t)
 
 
-@hydra.main(version_base=None, config_path="configs/", config_name="default")
-def main(cfg: Config):
+def main():
     times = np.arange(1, 200)
     tensors = np.array([[sin_func(times, 0.1 * i * j) for j in range(10)] for i in range(3)])
-    print("tensors=", tensors.shape)
     tensors = tensors.transpose(2, 1, 0)
     plot = TensorPlot()
+    plot.set_alpha(200)
     plot.plot_tensor(tensors, "outputs/test.png")
 
 
